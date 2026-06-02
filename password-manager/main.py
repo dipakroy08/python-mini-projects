e869136c5105c67042a387de3988318ed1395f2b
Dipak Kumar <dipakkumar202121@gmail.com>
2026-06-02 12:54:58 +0530

    final commit

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,5 +1,7 @@
 import random
 import string
+import os
+print(os.getcwd())
 
 passwords = {}
 
@@ -10,3 +12,48 @@ try:
         for line in file:
             website, pwd = line.strip().split(":")
             passwords[website] = pwd
+
+except Exception as e:
+    print("Error loading passwords:", e)
+    
+def generate_password():
+    chars = string.ascii_letters + string.digits + "!@#$%&"
+    password = "".join(random.choice(chars) for _ in range(8))
+    return password
+    
+while True:
+    print("\n----- PERSONAL PASSWORD MANAGER -----")
+    print("1. Save Password")
+    print("2. View Password")
+    print("3. Generate Password")
+    print("4. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+        site = input("Enter website: ")
+        pwd = input("Enter password: ")
+        
+        passwords[site] = pwd
+        
+        with open("passwords.txt", "a") as file:
+            file.write(f"{site}:{pwd}\n")
+            
+        print("Saved!")
+    
+    elif choice == "2":
+        if not passwords:
+            print("No data")
+        else:
+            for site, pwd in passwords.items():
+                print(site, ":", pwd)
+    
+    elif choice == "3":
+        print("Generated password", generate_password())
+        
+    elif choice == "4":
+        print("Exiting...")
+        break
+    
+    else:
+        print("Invalid input")
